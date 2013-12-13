3f6deec527f59f321507d37161d327a36c77082b
Laura <giuffridait@gmail.com>
2013-12-13 13:50:34 +0000

    First commit

---
+++ BeatYourPaceWindows/BeatYourPaceWindows/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BeatYourPaceWindows.Resources;
+
+namespace BeatYourPaceWindows
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
